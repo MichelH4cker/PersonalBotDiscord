c7b7d48b554aa85454d119ec081d8c3dbbf21494
Michel Hecker Faria <michel@pop-os.localdomain>
2021-09-03 22:33:33 -0300

    adding hello command

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import discord 
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = "-f", case_insensitive = True)
+client = commands.Bot(command_prefix = ">", case_insensitive = True)
 
 @client.event
 async def on_ready():
-    print('Entramos como {0.user}'.format(client))
+    print('Entramos como {0.user}' .format(client))
 
-client.run('ODgzNTA1NDM0NTcxNDY4ODEx.YTK6jg.8eqV8SuJwLUdspES3KKaogVHXlA')
+
+@client.command()
+async def ola(ctx):
+    await ctx.send(f'hello, {ctx.author}')
+
+client.run('ODgzNTA1NDM0NTcxNDY4ODEx.YTK6jg.bGiSAPLaIe6NUX5Ln25PKhy_z_U')
 
